6af1b40da5569604f6a7b1578baec0e69a2d8d16
Estherranipenke <96322095+Estherranipenke@users.noreply.github.com>
2023-07-19 12:16:12 +0530

    n=int(input()) length=len(str(n)) tem=n sum=0 rem=0 while tem>0:     rem=tem%10     sum=sum+int(rem**length)     tem=tem//10     length=length-1 if sum==n:     print("True") else:     print("False")

--- Disarium_number.py
+++ Disarium_number.py
@@ -1,15 +1,14 @@
 n=int(input())
-l=[]
-for i in str(n):
-    l.append(int(i))
-e=[]
-for j in range(1,len(l)+1):
-    e.append(j)
-z=list(zip(l,e))
-c=0
-for a,b in z:
-    c=c+a**b
-if c==n:
+length=len(str(n))
+tem=n
+sum=0
+rem=0
+while tem>0:
+    rem=tem%10
+    sum=sum+int(rem**length)
+    tem=tem//10
+    length=length-1
+if sum==n:
     print("True")
 else:
     print("False")
