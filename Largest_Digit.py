3bdc4b611931c7882a705e0dd2fd28818140e392
Estherranipenke <96322095+Estherranipenke@users.noreply.github.com>
2023-11-07 22:40:50 +0530

    num=int(input()) lar_dig=0 while num>0:     dig=num%10     if dig>lar_dig:         lar_dig=dig     num=num//10 print(lar_dig)

--- Largest_Digit.py
+++ Largest_Digit.py
@@ -1,9 +1,8 @@
-n=input()
-max=0
-for i in n:
-    i=int(i)
-    if i>=max:
-        max=i
-print(max)
-        
-      
+num=int(input())
+lar_dig=0
+while num>0:
+    dig=num%10
+    if dig>lar_dig:
+        lar_dig=dig
+    num=num//10
+print(lar_dig)
